30842de2c1718ae3b5f2a6c13d934e3a885a81cd
Kamil Samul <kamrep7@wp.pl>
2018-02-20 11:47:00 +0100

    Api improve

--- server/server.js
+++ server/server.js
@@ -24,7 +24,10 @@ app.post('/images', imagesUpload(
     './static/' + IMAGES,
     HTTP_SERVER_PORT_IMAGES
 ));
-
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
 
 app.get('/api/cities/getAllCities', (req, res) => {
     db.collection('cities').find().toArray()
@@ -35,34 +38,126 @@ app.get('/api/cities/getAllCities', (req, res) => {
         });
 });
 
-app.post('/api/cities/addCity', );
+app.post('/api/cities/addCity', function (req, res) {
+    db.collection('cities').insertOne(req.body, (err, result) => {
+        if(err)
+            res.send("Error");
+        else
+            res.send("Success");
+    });
+});
 
-app.post('/api/cities/removeCity', );
+app.post('/api/cities/removeCity', (req, res) => {
+    db.collection('cities').removeOne({_id: ObjectID(req.body.id)}, {safe: true}, (err, result) =>{
+        if(err)
+            res.send("Error");
+        else
+            res.send("Success");
+    });
+});
 
-app.post('/api/cities/getCity', );                  //Get specific city
+app.post('/api/cities/getCity', (req, res) => {
+    db.collection('cities').findOne({_id: ObjectID(req.body.id)})
+        .then(city => res.json(city))
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({message: `Internal Server Error : ${error}`});
+        });
+});                  //Get specific city
 
-app.get('/api/activities/getAllActivities', );
+app.get('/api/activities/getAllActivities', (req, res) => {
+    db.collection('activities').find().toArray()
+        .then(act => res.json(act))
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({message: `Internal Server Error : ${error}`});
+        });
+});
 
-app.get('/api/activities/getAllEvents', );
+app.get('/api/activities/getAllEvents', (req, res) => {
+    db.collection('activities').find({nature: "event"}).toArray()
+        .then(act => res.json(act))
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({message: `Internal Server Error : ${error}`});
+        });
+});
 
-app.get('/api/activities/getAllPlaces', );
+app.get('/api/activities/getAllPlaces', (req, res) => {
+    db.collection('activities').find({nature: "place"}).toArray()
+        .then(act => res.json(act))
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({message: `Internal Server Error : ${error}`});
+        });
+});
 
-app.get('/api/activities/getAllMonuments', );
+app.get('/api/activities/getAllMonuments', (req, res) => {
+    db.collection('activities').find({nature: "monument"}).toArray()
+        .then(act => res.json(act))
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({message: `Internal Server Error : ${error}`});
+        });
+});
 
-app.post('/api/activities/getActivity', );           //Get specific activity
+app.post('/api/activities/getActivity', (req, res) => {
+    db.collection('activities').findOne({_id: req.body.id})
+        .then(act => res.json(act))
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({message: `Internal Server Error : ${error}`});
+        });
+});           //Get specific activity
 
-app.get('/api/comments/getComments', );
+app.get('/api/comments/getComments', (req, res) => {
+    db.collection('comments').find().toArray()
+        .then(com => res.json(com))
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({message: `Internal Server Error : ${error}`});
+        });
+});
 
-app.post('/api/comments/addComment', );
+app.post('/api/comments/addComment', (req, res) => {
+    db.collection('comments').insertOne(req.body, (err, result) => {
+        if (err)
+            res.send("Error");
+        else
+            res.send("Success");
+    });
+});
 
-app.post('/api/comments/removeComment', );
+app.post('/api/comments/removeComment', (req, res) => {
+    bd.collection('comments').removeOne({_id: ObjectID(req.body.id)}, (err, result) => {
+        if (err)
+            res.send("Error");
+        else
+            res.send("Success");
+    });
+});
 
-app.post('/api/likes/addLike', );
+app.post('/api/likes/addLike', (req, res) => {
+    bd.collection('comments').removeOne({_id: ObjectID(req.body.id)}, (err, result) => {
+        if (err)
+            res.send("Error");
+        else
+            res.send("Success");
+    });
+});
 
-app.post('/api/likes/removeLike', );
+app.post('/api/likes/removeLike', (req, res) => {
 
-app.post('/api/user/validate', );
+});
 
-app.post('/api/user/register', );
+app.post('/api/user/validate', (req, res) => {
 
-app.post('/api/user/addAdminPermission', );
+});
+
+app.post('/api/user/register', (req, res) => {
+
+});
+
+app.post('/api/user/addAdminPermission', (req, res) => {
+
+});
