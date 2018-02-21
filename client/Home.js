d662d45ef1dfc72646d47f9cfbaea055f51c75b2
Mehdi <mehdi@leni-144-90.irsf>
2018-02-21 17:31:07 +0100

    front end

--- client/Home.js
+++ client/Home.js
@@ -8,32 +8,30 @@ import 'react-images-uploader/font.css';
 import {HTTP_SERVER_PORT_IMAGES} from '../server/constants';
 
 
-class CityLaconic extends React.Component{
-    render(){
-        return(
+class CityLaconic extends React.Component {
+    render() {
+        return (
+            <div>
+                <div>
+                    <span><Link to={'/city/' + this.props.city._id}>{this.props.city.name}</Link></span>
+                    <img src={this.props.city.picture} alt=""/>
+                </div>
+            </div>
+        )
+    }
+}
+
+class City extends React.Component {
+    render() {
+        return (
             <div className="card">
                 <div className="container">
-                    <h4><Link to={'/city/' + this.props.city._id}>{this.props.city.name}</Link></h4>
-                    <img width="250" height="250"  src={this.props.city.picture} alt="" />
+                    <h4><b>{this.props.name}</b></h4>
                 </div>
-
             </div>
         )
     }
 }
-
-class City extends React.Component{
-    render(){
-        return(
-        <div className="card">
-            <div className="container">
-                <h4><b>{this.props.name}</b></h4>
-            </div>
-        </div>
-        )
-    }
-}
-
 
 
 export default class Home extends React.Component {
@@ -41,6 +39,7 @@ export default class Home extends React.Component {
         super(props);
         this.state = {cities: [], name: "", lat: null, long: null, country: ""};
     }
+
     loadData() {
         fetch('/api/cities/getAllCities')                       // Ask the route /cities to the server
             .then(res => res.json())                       // Retrieve the objects  in json
@@ -67,7 +66,7 @@ export default class Home extends React.Component {
                 res.json().then(err => alert("Failed to add city: " + err.message));
         }).catch(err => alert("Error in sending data to server: " + err.message));
 
-        this.setState({name: "", lat: null, long:null});
+        this.setState({name: "", lat: null, long: null});
     }
 
     handleNameChange(e) {
@@ -81,40 +80,87 @@ export default class Home extends React.Component {
     handleLongChange(e) {
         this.setState({long: e.target.value});
     }
+
     handleCountryChange(e) {
         this.setState({country: e.target.value});
     }
 
-    componentDidMount(){
+    componentDidMount() {
         this.loadData();
     }
 
     render() {
         let cities = this.state.cities;
-        if (cities == []){
+        if (cities == []) {
             return <div>No Image Loaded</div>
-        }else {
+        } else {
             return (
                 <div>
-                    <h1>My Cities... The places to be!</h1>
-                    <p> You can find in this website many cities with beautiful places, events (festivals, concerts and
-                        so on).
-                        Please, join us, and you will have the possibilities to participate to this new social
-                        network. <br/>
-                        Enjoy!!
-                    </p>
-                    {cities.map(city => <CityLaconic key={city._id} city={city}></CityLaconic>)
-                    }
+                    <div id="about" className="pres-size">
+                        <div className="pres">
+                            <div>
+                                <img src="images/element1.png" alt=""/>
+                                <h1>Events</h1>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium architecto
+                                    assumenda consequatur
+                                    dolorum ex expedita</p>
+                                <a className="button1" href="">See more</a>
+                            </div>
+                            <div>
+                                <img src="images/element2.png" alt=""/>
+                                <h1>Monuments</h1>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium architecto
+                                    assumenda consequatur
+                                    dolorum ex expedita</p>
+                                <a className="button1" href="">See more</a>
+                            </div>
+                            <div>
+                                <img src="images/element3.png" alt=""/>
+                                <h1>Cities</h1>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium architecto
+                                    assumenda consequatur
+                                    dolorum ex expedita</p>
+                                <a className="button1" href="">See more</a>
+                            </div>
+                        </div>
+                    </div>
+                    <img className="separation" src="images/separation1.png" alt="separation"/>
+                    <span className="city_chose"> Choose your city ! </span>
+                    <div id="cities" className="city">
+                        {cities.map(city => <CityLaconic key={city._id} city={city}></CityLaconic>)}
+                    </div>
+                    <div className="footer-size">
+                        <div className="footer">
+                            <div className="form">
+                                <form onSubmit={(e) => this.addCity(e)}>
+                                    <input type="text" value={this.state.name} onChange={(e) => {
+                                        this.handleNameChange(e)
+                                    }} placeholder="Name of the city"/> <br/>
+                                    <input type="number" value={this.state.lat} onChange={(e) => {
+                                        this.handleLatChange(e)
+                                    }} placeholder="Latitude"/> <br/>
+                                    <input type="number" value={this.state.long} onChange={(e) => {
+                                        this.handleLongChange(e)
+                                    }} placeholder="Longitude"/> <br/>
+                                    <input type="text" value={this.state.country} onChange={(e) => {
+                                        this.handleCountryChange(e)
+                                    }} placeholder="Country"/> <br/>
 
-                    <h2>Insert a new city</h2>
-                    <form onSubmit={(e) => this.addCity(e)}>
-                        <input type="text" value={this.state.name} onChange={(e) => {this.handleNameChange(e)}} placeholder="Name of the city" /> <br />
-                        <input type="number" value={this.state.lat} onChange={(e) => {this.handleLatChange(e)}} placeholder="Latitude" /> <br />
-                        <input type="number" value={this.state.long} onChange={(e) => {this.handleLongChange(e)}} placeholder="Longitude" /> <br />
-                        <input type="text" value={this.state.country} onChange={(e) => {this.handleCountryChange(e)}} placeholder="Country" /> <br />
-
-                        <input type="submit" value="Create" />
-                    </form>
+                                    <input type="submit" value="Create"/>
+                                </form>
+                            </div>
+                            <div>
+                                <h1>Add city your here, nothing easier</h1>
+                                <p>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis perspiciatis quis
+                                    magni impedit corporis
+                                    nam iusto tempora quod ipsam, veritatis aperiam. Alias deleniti ex dicta vel tempora
+                                    magni consequatur
+                                    laudantium?
+                                </p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             )
         }
--- client/main.js
+++ client/main.js
@@ -10,29 +10,74 @@ import Modal from './Modal.js';
 
 ReactDOM.render(
     <div>
-        <header>
-            <nav className="navigation">
+        <header id="home" className="header">
+            <nav id="menu">
+                <img src="/images/menu_open.png" alt="menu_open" className="menu_open"/>
+                <img src="/images/menu_close.png" alt="menu_close" className="menu_close"/>
+                <a href="#home"><img className="name" src="/images/Logo_AirnayBlanc.png"/></a>
                 <ul>
-                    <li className="menuobj"><p><img src="/images/siteres/logo.png"/></p></li>
-                    <li className="menuobj"><a href="/">Home</a></li>
-                    <li className="menuobj"><a href="/">Add a city</a></li>
-                    <li className="form"><form method="post" action="traitement.php">
-                        <input type="text" placeholder="Search"/>
-                        <input type="submit" value="Go" />
-                    </form></li>
-                    <li className="menuobj"><a href="/">Sign In</a></li>
-                    <li className="menuobj"><a href="/">Log In</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#cities">Cities</a></li>
+                    <li><a href="#section_monuments">Monuments</a></li>
+                    <li><a href="#section_events">Events</a></li>
+                    <li><a href="/"><i className="fa fa-user" aria-hidden="true"></i>&nbsp;&nbsp;&nbsp;&nbsp;Julien
+                        Fovelle</a></li>
                 </ul>
             </nav>
 
+            <div id="slider">
+                <div id="slides">
+                    <img src="images/slider1.jpg" alt=""/>
+                    <img src="images/slider2.jpg" alt=""/>
+                    <img src="images/slider3.jpg" alt=""/>
+                    <img src="images/slider4.jpg" alt=""/>
+                    <img src="images/arras.png" alt=""/>
+                </div>
+                <span className="overlay"></span>
+            </div>
+
+            <div className="Content_Header">
+                <div className="discover">
+                    <span className="trait" id="traitG"></span>
+                    <span>DISCOVER</span>
+                    <span className="trait" id="traitD"></span>
+                </div>
+                <div class="header_txt">
+                    <span className="hightitle_header"> Powerful Features </span>
+                    <span className="subtitle_header"> Discover incredible places and events </span>
+                </div>
+            </div>
+
+            <div className="mouse_bounce">
+                <a href="#about"><img src="images/mouse.png"/></a>
+            </div>
         </header>
+
         <Router history={hashHistory}>
             <Route path="/" component={Home}/>
-            <Route path="/city/:id" component={City} />
-            <Route path="/activity/:id" component={Activity} />
+            <Route path="/city/:id" component={City}/>
+            <Route path="/activity/:id" component={Activity}/>
             <Route path="*" component={() => <p>Page Not Found</p>}/>
         </Router>
-        <footer> Footer</footer>
+
+        <footer>
+            <div className="footer_corps">
+                <div className="footer_copyright">
+                    Copyright - Prompt Agency
+                </div>
+                <div className="footer_navigation">
+                    <ul>
+                        <li><a href="#home">Home</a></li>
+                        <li><a href="#cities">Cities</a></li>
+                        <li><a href="#">More Informations</a></li>
+
+                        <li><a href="#"><i className="fa fa-facebook"></i></a></li>
+                        <li><a href="#"><i className="fa fa-twitter"></i></a></li>
+                        <li><a href="#"><i className="fa fa-linkedin"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </footer>
     </div>
     ,
     document.getElementById('root')
