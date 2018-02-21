417226ccfca934de5956b47a07e3741fff27914e
Polite <sitsha.polite@yahoo.ie>
2018-02-21 12:05:22 +0000

    Client Side Code

---
+++ client/Activity.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import {Link} from 'react-router';
+
+import ImagesUploader from 'react-images-uploader';
+import 'react-images-uploader/styles.css';
+import 'react-images-uploader/font.css';
+import Modal from './Modal.js';
+
+import {HTTP_SERVER_PORT_IMAGES} from '../server/constants'
+
+class Comment extends React.Component{
+    render(){
+        const d = new Date(this.props.com.date);
+        console.log(d.getFullYear(), d.getDate());
+        return(
+            <div className='comment'>
+                <p>{this.props.com.text}</p>
+                <p>{d.getFullYear()} , {d.getMonth()+1},{d.getDate()}</p>
+            </div>
+        )
+    }
+}
+
+class Pic extends React.Component{
+    render(){
+        return(
+            <div className='pic'>
+                <img src= {this.props.pic} width="250px" height="250px"/>
+            </div>
+        )
+    }
+}
+
+export default class Activity extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            activity: null,
+            isOpen:false
+        }
+        this.toggle=this.toggle.bind(this);
+    };
+
+    loadData() {
+
+        fetch('/api/activities/getAllActivities'+this.props.params.id)                       // Ask the route /cities to the server
+            .then(res => res.json())                       // Retrieve the objects  in json
+            .then(data => this.setState({activity: data}))   // Modify the state accordingly
+            .catch(err => console.log(err));               // Bad news: an error!
+
+    }
+
+    componentDidMount() {
+        this.loadData();
+    }
+
+    toggle(){
+        this.setState({isOpen: !this.state.isOpen});
+    }
+
+
+    render() {
+        let activity =  this.state.activity;
+        if(activity == null){
+            return ( <div>loading</div>)
+        }
+        else {
+            return (
+                <div className='activity'>
+                    {this.state.activity.pictures.map((a,i) => <Pic pic={a}/> )}
+                    <p>{this.state.activity.name}</p>
+                    <p>{this.state.activity.description}</p>
+                    {this.state.activity.comments.map((a,i) => <Comment com={a}/> )}
+                    <p><a id="open" onClick={(e)=>this.toggle(e)}>Modaltest</a> </p>
+                    <Modal isOpen={this.state.isOpen} toggle={this.toggle}>
+                        <h1> Close </h1>
+                    </Modal>
+
+
+                </div>
+
+            )
+        }
+    }
+}
+
+
---
+++ client/City.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import {Link} from 'react-router';
+
+import ImagesUploader from 'react-images-uploader';
+import 'react-images-uploader/styles.css';
+import 'react-images-uploader/font.css';
+
+import {HTTP_SERVER_PORT_IMAGES} from '../server/constants'
+
+class Activity extends React.Component{
+    render(){
+        return(
+            <div className='activities'>
+                <img src= {this.props.activity.picture} width="250px" height="250px"/>
+                <p><Link to={`/activity/${this.props.activity._id}`} activeClassName="active">{this.props.activity.name}</Link></p>
+            </div>
+        )
+    }
+
+}
+
+export default class City extends React.Component {
+    constructor(props) {
+        super(props);
+        console.log('AAAA');
+
+        this.state = {
+            city: null
+        }
+        this.loadData();
+
+    };
+
+    loadData() {
+        console.log('/api/cities/getCity/'+this.props.params.id);
+        fetch('/api/cities/getCity/'+this.props.params.id)                       // Ask the route /cities to the server
+            .then(res => res.json())                       // Retrieve the objects  in json
+            .then(data => this.setState({city: data}))   // Modify the state accordingly
+            .catch(err => console.log(err));               // Bad news: an error!
+        console.log(this.state.city);
+
+    }
+
+    componentDidMount() {
+        this.loadData();
+    }
+
+
+    render() {
+        let city =  this.state.city;
+        console.log(city);
+        if(city == null){
+            return ( <div>loading...</div>)
+        }else {
+            console.log(this.state.city);
+            return (
+                <div className='city'>
+                    <img src={this.state.city.picture}/>
+                    <p>{this.state.city.name}</p>
+                    <p>{this.state.city.description}</p>
+                    <h1> Places </h1>
+                    {this.state.city.activities.filter(a => a.nature=='place').map((a,i) => <Activity activity={a}/> )}
+                    <h1> Events </h1>
+                    {this.state.city.activities.filter(a => a.nature=='event').map((a,i) => <Activity activity={a}/>)}
+
+                </div>
+
+            )
+        }
+    }
+}
